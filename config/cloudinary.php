f593762ca48a7261059f4ecb0a3e8e1fbc8caf79
Legendaryl <darylsumabal123@gmail.com>
2025-12-29 15:44:52 +0800

    cloudinary

--- config/cloudinary.php
+++ config/cloudinary.php
@@ -32,11 +32,11 @@ return [
     |
     */
 
-    // 'cloud' => [
-    //     'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
-    //     'api_key'    => env('CLOUDINARY_API_KEY'),
-    //     'api_secret' => env('CLOUDINARY_API_SECRET'),
-    // ],
+    'cloud' => [
+        'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
+        'api_key'    => env('CLOUDINARY_API_KEY'),
+        'api_secret' => env('CLOUDINARY_API_SECRET'),
+    ],
 
     'cloud_url' => env('CLOUDINARY_URL', 'cloudinary://' . env('CLOUDINARY_KEY') . ':' . env('CLOUDINARY_SECRET') . '@' . env('CLOUDINARY_CLOUD_NAME')),
 
