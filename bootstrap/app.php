f7d69a38851a404e8f78c2a711188c99b40870b5
Legendaryl <darylsumabal123@gmail.com>
2025-12-29 10:57:59 +0800

    origins fix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         channels: __DIR__.'/../routes/channels.php',
         health: '/up',
-    )
+    )->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
+    })
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
 
