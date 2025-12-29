c1529b0064ed10a68a8e253b908c9b3d544f3c6f
Legendaryl <darylsumabal123@gmail.com>
2025-12-29 16:49:16 +0800

    pathName

--- app/Http/Controllers/Event/AddEventController.php
+++ app/Http/Controllers/Event/AddEventController.php
@@ -30,7 +30,7 @@ class AddEventController extends Controller
 
         if ($request->hasFile('poster')) {
             $cloudPath = Storage::disk('cloudinary')
-                ->put('poster', $request->file($posterPath));
+                ->put('poster', $request->file('poster'));
 
             $validate['poster'] = $cloudPath;
         }
