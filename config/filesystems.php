433632f1cd4af7ae0eef227393b4c0e744b77e4d
Legendaryl <darylsumabal123@gmail.com>
2025-12-29 15:46:53 +0800

    cloudinary

--- config/filesystems.php
+++ config/filesystems.php
@@ -64,7 +64,7 @@ return [
             'key' => env('CLOUDINARY_KEY'),
             'secret' => env('CLOUDINARY_SECRET'),
             'cloud' => env('CLOUDINARY_CLOUD_NAME'),
-            'url' => env('CLOUDINARY_URL'),
+            // 'url' => env('CLOUDINARY_URL'),
             // 'secure' => (bool) env('CLOUDINARY_SECURE', true),
             // 'prefix' => env('CLOUDINARY_PREFIX'),
         ],
