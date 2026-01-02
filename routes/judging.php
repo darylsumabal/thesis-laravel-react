dd4814483375deffa17c826403cf50955a5aac18
Legendaryl <darylsumabal123@gmail.com>
2026-01-02 16:30:49 +0800

    fix middleware

--- routes/judging.php
+++ routes/judging.php
@@ -10,14 +10,13 @@ Route::prefix('judging')->group(function () {
     Route::post('/', [JudgingController::class, 'storeJudge']);
 
     Route::delete('/{id}', [AccountController::class, 'destroy']);
+    Route::post('/edit-score/{contestId}/{groupId}/{judgeId}', [JudgingController::class, 'updateFinishedUpdate']);
 
     Route::middleware('is_judge')->group(function () {
-        Route::post('/edit-score/{contestId}/{groupId}/{judgeId}', [JudgingController::class, 'updateFinishedUpdate'])->middleware('is_judge');
+        Route::get('/contest-list', [CriteriaController::class, 'indexCriteriaTable'])->name('judgesTable');
 
-        Route::get('/contest-list', [CriteriaController::class, 'indexCriteriaTable'])->name('judgesTable')->middleware('is_judge');
+        Route::post('/score/{judgeId}/{contestId}/{groupId}/{roundType}', [JudgingController::class, 'storeJudging']);
 
-        Route::post('/score/{judgeId}/{contestId}/{groupId}/{roundType}', [JudgingController::class, 'storeJudging'])->middleware('is_judge');
-
-        Route::get('/criteria-list/{contestId}/{groupId}', [JudgingController::class, 'indexCriteria'])->middleware('is_judge');
+        Route::get('/criteria-list/{contestId}/{groupId}', [JudgingController::class, 'indexCriteria']);
     });
 });
