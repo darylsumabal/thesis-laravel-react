a093f241d177489a4bedcc0ab73df10aeebce7da
Legendaryl <darylsumabal123@gmail.com>
2026-01-02 12:26:24 +0800

    app name

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -19,7 +19,8 @@ configureEcho({
     enabledTransports: ['ws', 'wss'],
 });
 
-const appName = import.meta.env.APP_NAME || 'Laravel';
+// const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = "Judging Tabulation";
 
 const queryClient = new QueryClient({
     defaultOptions: {
