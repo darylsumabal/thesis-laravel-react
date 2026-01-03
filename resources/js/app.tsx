af643dd6235f53e9e7e36b4b34de4a48ab0d53af
Legendaryl <darylsumabal123@gmail.com>
2026-01-03 11:36:20 +0800

    pusher

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,13 +1,12 @@
 import '../css/app.css';
 
 import { createInertiaApp } from '@inertiajs/react';
-import { configureEcho } from '@laravel/echo-react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
 import { Toaster } from './components/ui/sonner';
 import { ContextProvider } from './context/ContesxtProvider';
 import { initializeTheme } from './hooks/use-appearance';
+import { configureEcho } from '@laravel/echo-react';
 
 // configureEcho({
 //     broadcaster: 'reverb',
@@ -19,29 +18,36 @@ import { initializeTheme } from './hooks/use-appearance';
 //     enabledTransports: ['ws', 'wss'],
 // });
 
-configureEcho({
-    broadcaster: 'reverb',
-    key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: import.meta.env.VITE_REVERB_PORT ?? 443,
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
-    forceTLS: true,
-    enabledTransports: ['ws', 'wss'],
-});
-console.log('VITE_REVERB_APP_KEY:', import.meta.env.VITE_REVERB_APP_KEY);
-console.log('VITE_REVERB_HOST:', import.meta.env.VITE_REVERB_HOST);
-console.log('VITE_REVERB_PORT:', import.meta.env.VITE_REVERB_PORT);
+// configureEcho({
+//     broadcaster: 'reverb',
+//     key: import.meta.env.VITE_REVERB_APP_KEY,
+//     wsHost: import.meta.env.VITE_REVERB_HOST,
+//     wsPort: import.meta.env.VITE_REVERB_PORT ?? 443,
+//     wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
+//     forceTLS: true,
+//     enabledTransports: ['ws', 'wss'],
+// });
+// console.log('VITE_REVERB_APP_KEY:', import.meta.env.VITE_REVERB_APP_KEY);
+// console.log('VITE_REVERB_HOST:', import.meta.env.VITE_REVERB_HOST);
+// console.log('VITE_REVERB_PORT:', import.meta.env.VITE_REVERB_PORT);
 
 // const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 const appName = 'Judging Tabulation';
 
-const queryClient = new QueryClient({
-    defaultOptions: {
-        queries: {
-            staleTime: 1000 * 60, // 1 minute
-            refetchOnWindowFocus: false,
-        },
-    },
+// const queryClient = new QueryClient({
+//     defaultOptions: {
+//         queries: {
+//             staleTime: 1000 * 60, // 1 minute
+//             refetchOnWindowFocus: false,
+//         },
+//     },
+// });
+
+configureEcho({
+    broadcaster: 'pusher',
+    key: import.meta.env.VITE_PUSHER_APP_KEY,
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+    forceTLS: true,
 });
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
@@ -49,12 +55,12 @@ createInertiaApp({
     setup({ el, App, props }) {
         const root = createRoot(el);
         root.render(
-            <QueryClientProvider client={queryClient}>
+            <>
                 <Toaster richColors closeButton position="top-center" />
                 <ContextProvider>
                     <App {...props} />
                 </ContextProvider>
-            </QueryClientProvider>,
+            </>,
         );
     },
     progress: {
@@ -62,11 +68,4 @@ createInertiaApp({
     },
 });
 
-// This will set light / dark mode on load...
 initializeTheme();
-
-// "dev:ssr": [
-//     "npm run build:ssr",
-//     "Composer\\Config::disableProcessTimeout",
-//     "npx concurrently -c \"#93c5fd,#c4b5fd,#fb7185,#fdba74\" \"php artisan serve --host=0.0.0.0 --port=8000\" \"php artisan queue:listen --tries=1\" \"php artisan pail --timeout=0\" \"php artisan inertia:start-ssr\" \"php artisan reverb:start\" --names=server,queue,logs,ssr,reverb"
-// ],
